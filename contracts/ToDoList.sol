4cf0d671dce2e61a1a80704fd22bbadfa0db88b3
DaniTechDev <tochidan11@gmail.com>
2024-09-11 08:23:04 -0700

    Done with tthe Todolist struct

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -23,5 +23,51 @@ contract Todolist {
         bool completed
     )
 
-    mapping (address => TodoListApp) public ToDoListApp;
+    mapping (address => TodoListApp) public toDoListApps;
+
+    constructor() {
+        ownerOfContract = msg.sender;
+    }
+
+    function inc() internal {
+        _idUser++;
+    }
+
+    function createList(string calldata _message) external {
+        inc();
+
+        uint256 idNumber = _idUser;
+        TodoListApp storage toDo = toDoListApps[msg.sender];
+
+        todo.account = msg.sender;
+        toDo.message = _message;
+        toDo.completed = false;
+        toDo.userId = idNumber
+
+        creators.push(msg.sender);
+        message.push(_message);
+        messageId.push(idNumber);
+        
+        emit ToDoEvent(msg.sender,toDo.userId,_message,toDo.completed)
+    }
+
+    function getCreatorData(address _address ) public view returns(address, uint,string memory, bool){
+        TodoListApp memory singleUserData = toDoListApps[_addresss];
+
+        return (
+            singleUserData.account,
+            singleUserData.userId,
+            singleUserData.messsage,
+            singleUserData.completed
+
+        )
+    }
+
+    //function to get all the arrays we created
+
+    function getAddresses() external view returns (address[] memory){
+        return creators;
+    }
+
+    functiom
 }
