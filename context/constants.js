1df273f246921d7de8b97c63027bcac914aea740
DaniTechDev <tochidan11@gmail.com>
2024-09-14 04:36:02 -0700

    Imported todoList abi and address after deployment into out constant file

--- context/constants.js
+++ context/constants.js
@@ -1,1 +1,7 @@
 //contract addresss: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import ToDoList from "./ToDoList.json";
+
+export const todoListAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const todoListABI = ToDoList.abi;
