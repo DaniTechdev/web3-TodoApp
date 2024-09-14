a726cb2454ab0f37feea4cc0cbabb11c37ad7364
DaniTechDev <tochidan11@gmail.com>
2024-09-14 05:54:10 -0700

    Check for the working of the context api,works!!!

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 
+//INTERNAL IMPORT
+import { ToDoListContext } from "../context/ToDolistApp";
 const Home = () => {
+  const { checkIfWalletIsConnected } = useContext(ToDoListContext);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
   return <div>Home</div>;
 };
 
